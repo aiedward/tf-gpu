cd3a9b1878ce6e133eeb069c6a84f1d81203a897
arfu <arfu.guo@gmail.com>
2019-01-02 15:47:33 +0800

    Update word_vector_v1.py

--- word_vector_v1.py
+++ word_vector_v1.py
@@ -7,6 +7,7 @@
 """
 import tensorflow as tf
 from vocab_v2 import Vocab
+import config
 
 
 def text_to_ids(source_text, source_vocab_to_int):
@@ -22,15 +23,15 @@ def text_to_ids(source_text, source_vocab_to_int):
     return source_text_ids
 
 
-def embed(p_place):
+def embed(vocab, p_place):
     """
 
+    :param vocab: Vocab()
     :param p_place: placeholder
     p = tf.placeholder(tf.int32, [None, None])
     batch_size and sentence length
     :return: a tensor with variable and placeholder
     """
-    vocab = Vocab()
     with tf.device('/cpu:0'), tf.variable_scope('word_embedding'):
         # 此处指定使用cpu
         # 第一次建立这个variable_scope, 而不是reuse
@@ -48,3 +49,25 @@ def embed(p_place):
         # and Tensorflow?
         # https://stackoverflow.com/questions/34236252/difference-between-np-mean-and-tf-reduce-mean-in-numpy-and-tensorflow
         return s_emb
+
+
+if __name__ == "__main__":
+    vocab_main = Vocab()
+    vocab_main.load_vocab_from_embedding(config.embedding_path_air)
+    vocab_main.load_pretrained_embeddings(config.embedding_path_air)
+
+    sentence = "我 是 谁"
+    sentence_ids = text_to_ids(sentence, vocab_main.token2id)
+
+    sentence_place = tf.placeholder(tf.int32, [None, None])
+    embed_main = embed(vocab_main, sentence_place)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        vector = sess.run(embed_main, feed_dict={
+            sentence_place: sentence_ids
+        })
+        # print("list of vectors: ", vector)
+        print("type of vector:", type(vector[0]))
+        print("vector length:", vector[0].shape)
